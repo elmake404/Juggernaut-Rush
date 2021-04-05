ab3fe9286b05b1e1693ccf37fe562c9e2634c62b
Valentin <kibirevvalik202@gmail.com>
2021-04-05 16:16:25 +0300

    [add] Player Life

---
+++ Juggernaut-Rush/Assets/_scripts/CameraMove.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMove : MonoBehaviour
+{
+    [SerializeField]
+    private float _smoothTime;
+    private PlayerMove _target;
+    private Vector3 _velocity, _offset;
+
+    private void Start()
+    {
+        _target = FindObjectOfType<PlayerMove>();
+        _offset = transform.position - _target.transform.position;
+    }
+
+    private void FixedUpdate()
+    {
+        if (_target.GetLife())
+        {
+            transform.position = Vector3.SmoothDamp(transform.position, _target.transform.position + _offset, ref _velocity, _smoothTime);
+        }
+    }
+}
